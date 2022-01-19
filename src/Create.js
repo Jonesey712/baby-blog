ee101b818752f1347deb3f9806fb5570229dbf95
mjones <monique.jones@sprydigital.com>
2022-01-19 09:09:18 -0600

    Blog details and part of Create

---
+++ src/Create.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Jones');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then()
+    }
+}
